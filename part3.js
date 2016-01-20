71fdca0972bb3c7fecfd436187024ddfccf37b5d
mobaobaoss <bbzhanss@sina.com>
2016-01-21 03:50:11 +0800

    Update part3.js

--- part3.js
+++ part3.js
@@ -18,12 +18,12 @@ $(".examLi").each(function(){
     allQ.push($(this).text());
 });
 var counter = 0;
-for(var i = 1; i <= 30; i++){
+for(var i = 1; i <= 20; i++){
     var thisQ = allQ[i].split(" ");
     var q = thisQ[364].substring(0, thisQ[364].length - 1); //问题
     //答案
     var a = [];
-    for(var ii = 712; ii <= 772; ii += 30){
+    for(var ii = 712; ii <= 772; ii += 20){
         a.push(thisQ[ii].substring(1, thisQ[ii].length - 1));
     }
     var rightA = getAns(q); //获取正确答案
