400c191aa6aa6b45021a1a4dde7fcc888752deeb
mobaobaoss <bbzhanss@sina.com>
2016-01-22 06:45:38 +0800

    Update dati.js

--- dati.js
+++ dati.js
@@ -1,61 +1,136 @@
-var t = "\n\n欢迎您加入我们的群一起讨论并完善题库！\n一群：204524182\n二群：122214155";
-var p = document.createElement("script");
-p.setAttribute("src", "http://mobaobaoss.github.io/timu.js");
-p.setAttribute("charset", "utf-8");
-document.getElementsByTagName("head")[0].appendChild(p);
-if(r == "Google Chrome 40"){
-  var g = 7;
-}else if(r == "Internet Explorer 11"){
-  var g = 6;
-}else{
-  alert("启动脚本变量错误，请到我们的网站获取最新的启动脚本：\nhttp://x01x012013.github.io/CN_Pages/Bili_Macro/");
-  var g = prompt("请输入您浏览器\"L_0\"物体的innerHTML被\">\"split后第一个选项的index：", "7");
-  g = parseInt(g);
-  if(g === NaN){
-    g = 7;
-  }
-}
-var z = function(x, y){
-  document.getElementById("rdo" + x + y).checked = "checked";
-}
-p.onload = function(){
-  alert("Bilibili自动答题脚本 V3.2.3（2015.02.21更新）\n" + v + "\n脚本有可能需要一段时间才能完成执行，若您的浏览器提示脚本或网页长时间无响应请选择等待，请不要重复运行本脚本。" + t + "\n\n如果您为使用此脚本支付了任何费用，那么恭喜您，您被坑了。\n\n按确定开始执行。")
-  var c = 0;
-  var d = 0;
-  var f = confirm("若有题没有找到答案或答案无效，是否自动填选第一个选项（A）？选择否将会留空。\n确定：是；取消：否。");
-  try{
-    for(i = 0; i < 100; i++ ){
-      var l = document.getElementById("l_" + i).innerHTML.split(">");
-      var n = l[3].split("<");
-      n = n[0].substring(1, n[0].length - 8);
-      var a = b(n);
-      if(a !== undefined){
-        for(j = 0; j < 5; j += 1){
-          if(j === 4){
-            alert("题库错误，若您使用的是" + r + "或更新的版本的浏览器：请截图这个弹窗，然后找到这个问题并将问题和其答案都复制发给我。如果您知道如何查看网页源码，请找到该题和其答案相对应的源码并复制发给我（建议使用QQ群的临时会话，否则有可能出现特殊字符失效等问题）。\n\n问题：\"" + n + "\"\n返回的答案：\"" + a + "\"\n最后一次判定调用：\"" + s + "\"\n缓存：\"" + l + "\"");
-            if(f){
-              z(i, 0);
-            }
-            d += 1;
-            break;
-          }
-          var s = l[g + (j * 3)].split("<");
-          s = s[0].substring(0, s[0].length - 9);
-          if(s === a){
-            z(i, j);
-            break;
-          }
-        }
-      }else{
-        if(f){
-          z(i, 0);
-        }
-        c += 1;
-      }
+/*
+更新日志：
+V1.0 2015.05.20
+1.完成基础框架。
+V1.1 2015.05.21
+1.大量细节调整
+*/
+try{ //系统错误处理
+prompt("我们的网站（包含脚本使用说明，使用本脚本前请务必阅读）：", "http://x01x012013.github.io/CN_Pages/Bili_Macro/");
+//URL检查
+if(document.URL === "https://account.bilibili.com/answer/promotion" || document.URL === "https://account.bilibili.com/answer/promotion/"){
+//运行确认
+if(confirm("Bilibili晋级考试 弹幕礼仪部分 答题脚本 V1.1（2015.05.21更新）\n点击确定运行（点击取消则不会运行）。")){
+alert("题库题数：25（2015.05.23更新）\n若您为使用此脚本支付了任何费用，那么恭喜您，您被坑了。\n点击确定继续。");
+//核心
+var allQ = [];
+$(".examLi").each(function(){
+    allQ.push($(this).text());
+});
+var counter = 0;
+for(var i = 1; i <= 30; i++){
+    var thisQ = allQ[i].split(" ");
+    var q = thisQ[64].substring(0, thisQ[64].length - 1); //问题
+    //答案
+    var a = [];
+    for(var ii = 112; ii <= 172; ii += 30){
+        a.push(thisQ[ii].substring(1, thisQ[ii].length - 1));
     }
-    alert("自动答题结束，有" + c + "道题没找到，有" + d + "道题返回无效（题库错误）。\n请您检查一遍并填写未找到的问题后提交。" + t);
-  }catch(e){
-    alert("发生错误，脚本将终止。\n若您在答题页面运行的这个脚本且您的浏览器是" + r + "或更新的版本，请截图并发给我。\n错误信息：\n" + e + "\n" + t);
-  }
-  alert("感谢这些人对我的大力支持（不分排名）：\n优子（*****5201）\nje（******4946） ");
+    var rightA = getAns(q); //获取正确答案
+    if(a.indexOf(rightA) > -1){
+        $(".examLi").eq(i) //找到正确的题
+            .find("li").eq(a.indexOf(rightA)) //找到正确的选项
+                .addClass("currSolution"); //选择选项
+    }else{
+        counter += 1;
+        if(rightA !== undefined){ //题库错误处理
+            alert("题库错误！\n问题：\"" + q + "\"\n返回的答案：\"" + rightA + "\"\n捕获的答案列表：" + a);
+        }
+    }
+}
+alert("答题完成，有" + counter  +"道题没填写（题库中没有或返回的答案无效）。")
+//题库函数
+function getAns (q){
+// 79 = 79
+switch(q){
+//群主提供 25题 2015.05.20
+case "以下哪一位数学家不是法国人？": return ".黎曼";
+case "当PCl5 = PCl3 +Cl2的化学平衡常数Kc=1.8M时，在0.5L的容器中加入0.15molPCl5,求平衡后Cl2的浓度": return ".0.26M";
+case "以下哪种颜色不属于色光三原色？": return ".黄";
+case "iphone 4之后苹果推出的手机是": return ".iphone 4S";
+case "c++的int类型通常占用几个字节？": return "4（byte）";
+case "冥王星的公转周期?": return ".248地球年";
+case "世界上用图像显示的第一个电子游戏是什么？": return ".Core War";
+case "AIDS的全称是什么?": return ".获得性免疫缺陷综合征";
+case "计算机编程中常见的if语句是？": return ".判断语句";
+case "风靡一时的游戏主机“红白机”全名为？": return ".Family Computer";
+case "当天空出现了鱼鳞云(透光高积云)，下列哪种天气现象会发生？": return ".降雨";
+case "一直被拿去做实验从未被超越的动物是": return ".小白鼠";
+case "以下作品中哪一个完全没有使用3D技术": return ".LOVELIVE TV版";
+case "以下哪款耳机采用了特斯拉技术": return ".拜亚T1";
+case "美国历史上第一位黑人总统是": return ".Barack Hussein Obama II";
+case "以下哪种功率放大器效率最低？": return ".甲类";
+case "RGBA中的A是指？": return ".Alpha";
+case "“不学礼，无以立”。出于何处？": return ".《论语》";
+case "静电场的高斯定理和环路定理说明静电场是个什么场？": return ".有源有旋场";
+case "新生物性状产生的根本原因在于？": return ".基因重组";
+case "一个农田的全部生物属于？": return ".群落";
+case "北回归线没有从下列哪个省中穿过？": return ".福建";
+case "什么是“DTS”": return ".数字家庭影院系统";
+case "被誉为生命科学“登月”计划的是": return ".人类基因组计划";
+case "下列有关电子显微镜的说法正确的是": return ".常用的有透射电镜和扫描电子镜";
+case "天文学上，红移是指": return ".天体离我们远去";
+case "传说中从天而降砸到牛顿的是": return ".苹果";
+case "拿破仑在从厄尔巴岛逃回法国，到兵败滑铁卢再次流放，一共重返帝位多少天？": return ".101";
+case "剧毒NaCN(氰化钠)的水解产物HCN是什么味道": return ".苦杏仁味";
+case "金鱼的卵什么颜色的才能孵化": return ".淡青色";
+case "C语言中，如果ar是个数组，那么表达式ar[i]与什么等价？": return ".*(ar+i)";
+case "谁罪大滔天，搞到百姓怨声载道": return ".索尼";
+case "春秋战国时期，哪家学说的主张是“兼爱”“非攻”": return ".墨家";
+case "薛定谔的猫是薛定谔的一个著名实验，其不涉及的问题是": return ".量子的叠加态";
+case "李靖的三儿子叫什么(三个字)": return ".李狗蛋";
+case "NTSC制式的视频帧率是": return ".29.970fps";
+case "日本剑圣宫本武藏的自创的武术是什么？": return ".二天一流";
+case "造成温室效应最重要的气体是？": return ".二氧化碳";
+case "纳米是一种？": return ".长度单位";
+case "iphone 4之后苹果推出的手机是": return ".iphone 4S";
+case "硝基盐酸为什么能溶解金？": return ".氧化";
+case "“夜阑卧听风吹雨，铁马冰河入梦来”是谁的诗句？": return ".陆游";
+case "IBM5100的CPU型号？": return ".P-III";
+case "经过减数分裂的子细胞, 会有多少染色体": return ".一半";
+case "游戏主机Wii U是哪家公司发售的": return ".任天堂";
+case "不属于热的传递方式的是": return ".热运送";
+case "以下哪种手机品牌由于质量过”硬“而获得了砖头的美誉？": return ".诺基亚";
+case "以下哪种花卉最容易被从中间爆开？": return ".菊花";
+case "电磁炮发射炮弹用到的原理是": return ".洛伦茨力";
+case "“疑是地上霜”的上一句是？": return ".床前明月光";
+case "量子力学的创始人是？": return ".普朗克";
+case "将一批粗铜矿石由昆明运到上海精炼，运输路线哪条最经济？": return ".贵昆线—川黔线—长江航线";
+case "AE中将要如何做出视频信号干扰以及画面抖动的效果？": return ".AE插件Twitch";
+case "以下哪一个是北京地方时经纬坐标？": return ".北纬39.9°，东经116.3°";
+case "以下那个Adobe的软件是用于剪辑视频的？": return ".Premiere";
+case "赤道半径长为？": return ".6378km";
+case "二战德国闪电战的创立者是谁？": return ".古德里安";
+case "著名的狼群战术是谁提出来的？": return ".卡尔·邓尼茨";
+case "光年为光在真空中一年内行经的距离，这里的一年为？": return "光年为光在真空中一年内行经的距离，这里的一年为？";
+case "以下那种波不是电磁波？": return ".超声波";
+case "孤岛危机又叫什么": return ".显卡危机";
+case "用鼠标在电脑上绘图叫做什么？": return ".鼠绘";
+case "我国4个直辖市中哪个面积最大？": return ".重庆";
+case "以下哪种力不属于性质力？": return ".压力";
+case "c语言中换行符是？": return ".\n";
+case "世界上第一家第三方游戏开发公司叫什么？": return ".KONAMI";
+case "截止于2012年为止，宇宙中已知的最大恒星是下列中的哪一个？": return ".VY红超巨星";
+case "地球距离月球之间的距离为？": return ".384,400千米";
+case "第六天魔王是？": return ".织田信长";
+case "Android操作系统是以什么为基础开发的操作系统？": return ".Linux";
+case "2003娘，或称2K3娘，是哪个公司公司推出的的拟人化萌娘。": return ".微软";
+case "DMSO的中文名称是？": return ".二甲基亚砜";
+case "下列哪一个检验是否具有人工智能的测试？": return ".图灵测试";
+case "以下哪种化♂学物质是存在的": return ".正戊烷";
+case "以下哪种花卉最容易被从中间爆开？": return ".菊花";
+case "下列哪个不是Microsoft产品": return ".Android";
+case "当一枚硬币加速到光速的8/9时,它": return ".质量比之前大";
+case "美国历史上第一位黑人总统是": return ".Barack Hussein Obama II";
+case "以下哪句不出自与“马嵬坡下黄土中，不见红颜空死处”同首诗？": return ".回眸一笑百媚生，此时无声胜有声";
+case "以下哪一种细胞器附着在内质网上？": return ".高尔基体";
+//
+default: return undefined;
+}} //结束switch和题库函数
+} //结束运行确认
+}else{ //URL检查
+    alert("请在正确的页面上运行脚本！");
+}
+}catch(e){ //系统错误处理
+    alert("系统错误！\n错误信息：\n" + e);
 }
